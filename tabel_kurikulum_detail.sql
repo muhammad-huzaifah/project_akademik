cf8208c7053918b8aa845991d77dd2d31c4a0e46
muhammad-huzaifah <huzaifah2009@gmail.com>
2022-12-11 18:34:35 +0700

    tabel_kurikulum_detail.sql

--- tabel_kurikulum_detail.sql
+++ tabel_kurikulum_detail.sql
@@ -1,3 +1,3 @@
-insert into akademik.tabel_kurikulum_detail (id_kurikulum_detail, id_kurikulum, kd_mapel, kd_jurusan, kelas)
-values  (1, 1, 'BID', 'RPL', 3),
-        (4, 1, 'TIK', 'RPL', 2);
+INSERT INTO akademik.tabel_kurikulum_detail (id_kurikulum_detail, id_kurikulum, kd_mapel, kd_jurusan, kelas) VALUES (4, 1, 'TIK', 'TKJ', 2);
+INSERT INTO akademik.tabel_kurikulum_detail (id_kurikulum_detail, id_kurikulum, kd_mapel, kd_jurusan, kelas) VALUES (7, 1, 'MTK', 'RPL', 2);
+INSERT INTO akademik.tabel_kurikulum_detail (id_kurikulum_detail, id_kurikulum, kd_mapel, kd_jurusan, kelas) VALUES (8, 3, 'TIK', 'TKJ', 3);
